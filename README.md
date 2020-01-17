fe909dae79fd1ff68c8e4302cb621283a4fbec5d
Jean Bisutti <jean.bisutti@gmail.com>
2020-01-17 12:41:42 +0100

    Add a project containing examples with Spring Boot and JUnit 4

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# quickperf-examples
+# QuickPerf examples
 
-This repository contains example of how to integrate Quickperf with some popular frameworks.
+This repository contains examples showing how to use [QuickPerf](https://github.com/quick-perf/quickperf) together with various frameworks (JUnit 4, JUnit 5, TestNG, Spring Boot, Micronaut, ...).
 
-It contains the following example applications:
-
+The following applications are available:
+- [springboot-junit4-examples](springboot-junit4-examples): This Spring Boot project illustrates how to use QuickPerf with JUnit 4. 
 - [micronaut-hibernate-jpa-example](micronaut-junit5-example): Micronaut Hibernate JPA with JUnit5 test using both QuickPerf JVM and SQL annotations. 
---
+++ springboot-junit4-examples/src/test/java/football/repository/PlayerRepositoryTest.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2020-2020 the original author or authors.
+ */
+
+package football.repository;
+
+import football.QuickPerfPerBeanConfig;
+import football.entity.Player;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.quickperf.annotation.DisableQuickPerf;
+import org.quickperf.annotation.FunctionalIteration;
+import org.quickperf.spring.junit4.QuickPerfSpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(QuickPerfSpringRunner.class)
+@Import(QuickPerfPerBeanConfig.class)
+@DataJpaTest()
+public class PlayerRepositoryTest {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    //@FunctionalIteration // You can use @FunctionalIteration or @DisableQuickPerf
+                           // to disable QuickPerf features and focus on functional
+                           // behavior (not performance behavior) in a first step.
+                           // In a second step, you can remove @FunctionalIteration
+                           // or @DisableQuickPerf to evaluate some performance
+                           // properties.
+                           // We recommend to do one step at a time.
+    // The test will fail because N+1 select is detected from
+    // disableSameSelectTypesWithDifferentParams defined in QuickPerfConfiguration
+    // class.
+    @Test
+    public void should_find_all_players() {
+        List<Player> players = playerRepository.findAll();
+        assertThat(players).hasSize(2);
+    }
+
+}
