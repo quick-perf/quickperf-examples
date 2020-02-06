5c494aacd4f54c35db02675fbdab2db7107a71ef
Jean Bisutti <jean.bisutti@gmail.com>
2020-02-06 12:20:13 +0100

    Check max query execution time in global annotations

--- springboot-junit4/src/test/java/org/quickperf/QuickPerfConfiguration.java
+++ springboot-junit4/src/test/java/org/quickperf/QuickPerfConfiguration.java
@@ -18,6 +18,7 @@ import org.quickperf.config.SpecifiableGlobalAnnotations;
 import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 
 import static org.quickperf.sql.annotation.SqlAnnotationBuilder.*;
 
@@ -30,20 +31,22 @@ public class QuickPerfConfiguration implements SpecifiableGlobalAnnotations {
     public Collection<Annotation> specifyAnnotationsAppliedOnEachTest() {
 
         return Arrays.asList(
-                  // Can reveal some N+1 selects
-                  // https://blog.jooq.org/2017/12/18/the-cost-of-jdbc-server-roundtrips/
-                  disableSameSelectTypesWithDifferentParams()
+                // Can reveal some N+1 selects
+                // https://blog.jooq.org/2017/12/18/the-cost-of-jdbc-server-roundtrips/
+                disableSameSelectTypesWithDifferentParams()
 
                 , // Sometimes, JDBC batching can be disabled:
-                  // https://abramsm.wordpress.com/2008/04/23/hibernate-batch-processing-why-you-may-not-be-using-it-even-if-you-think-you-are/
-                  // https://stackoverflow.com/questions/27697810/hibernate-disabled-insert-batching-when-using-an-identity-identifier
-                  expectJdbcBatching()
+                // https://abramsm.wordpress.com/2008/04/23/hibernate-batch-processing-why-you-may-not-be-using-it-even-if-you-think-you-are/
+                // https://stackoverflow.com/questions/27697810/hibernate-disabled-insert-batching-when-using-an-identity-identifier
+                expectJdbcBatching()
 
                 , // https://use-the-index-luke.com/sql/where-clause/searching-for-ranges/like-performance-tuning
-                  disableLikeWithLeadingWildcard()
+                disableLikeWithLeadingWildcard()
 
-                , // Can reveal a bad use of Hibernate session
-                  disableExactlySameSelects()
+                , disableExactlySameSelects()
+
+                // Not relevant with an in-memory database used for testing purpose
+                , expectMaxQueryExecutionTime( 30, TimeUnit.MILLISECONDS)
 
         );
 
