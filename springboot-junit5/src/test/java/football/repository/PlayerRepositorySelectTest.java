77e45f0353d328d2745a0d4d33b558ba7c53d2b6
Jean Bisutti <jean.bisutti@gmail.com>
2020-07-27 22:12:08 +0200

    Improve @ExpectJdbcBathing example

---
+++ springboot-junit5/src/test/java/football/repository/PlayerRepositorySelectTest.java
@@ -0,0 +1,51 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright 2020-2020 the original author or authors.
+ */
+
+package football.repository;
+
+import football.QuickPerfBeanConfig;
+import football.entity.Player;
+import football.entity.Team;
+import org.junit.jupiter.api.Test;
+import org.quickperf.junit5.QuickPerfTest;
+import org.quickperf.sql.annotation.DisplaySql;
+import org.quickperf.sql.annotation.DisplaySqlOfTestMethodBody;
+import org.quickperf.sql.annotation.ExpectJdbcBatching;
+import org.quickperf.sql.annotation.ExpectSelect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(QuickPerfBeanConfig.class)
+@DataJpaTest(properties = "spring.datasource.data=classpath:import-teams.sql"
+                        + ",classpath:import-players.sql")
+@QuickPerfTest
+public class PlayerRepositorySelectTest {
+
+    @Autowired
+    private PlayerRepository playerRepository;
+
+    @ExpectSelect(1)
+    @Test
+    public void should_find_all_players() {
+        List<Player> players = playerRepository.findAll();
+        assertThat(players).hasSize(2);
+    }
+
+}
